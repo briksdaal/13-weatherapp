300a619347d114b19acd5020fd9861a5e9baf456
briksdaal <02250834+briksdaal@users.noreply.github.com>
2023-04-03 19:43:34 +0300

    Create project files and install dependencies

---
+++ README.md
@@ -0,0 +1,6 @@
+Odin Weather App Assignment
+
+Build a weather app
+
+Receive a location form the client, make an async API call to receive location's weather,
+format the response, and present the data visually.
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+
+module.exports = {
+  mode: 'development',
+  optimization: {
+    usedExports: true,
+  },
+  entry: './src/index.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Weather App',
+    }),
+    // new BundleAnalyzerPlugin(),
+  ],
+  devServer: {
+    static: './dist',
+    watchFiles: ['src/**/*.js', 'src/**/*.css'],
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|webp|svg|jpg|jpeg|gif|avif|svg)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+};
